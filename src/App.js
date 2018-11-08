8b687bbdd51449fc8b81bcf001bb88c7f88ef14f
alex.villecco <alex.villecco@globant.com>
2018-11-08 09:15:17 -0300

    Set up Firebase Hosting and create login screen (Incompleted)

--- src/App.js
+++ src/App.js
@@ -3,25 +3,60 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      logged: false,
+      loggedJury: "",
+      input: '',
+      value: ''
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.updateInput = this.updateInput.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({
+      value: this.state.input,
+      logged: true
+    });
+  }
+
+  updateInput(event) {
+    this.setState({input: event.target.value});
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+
+    if(this.state.logged) {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Bienvenido { this.state.value }
+            </p>    
+          </header>
+        </div>
+      );
+    } else {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <p>
+              Ingresar
+            </p>
+            <form onSubmit={this.handleSubmit}>
+              <input type="text" placeholder="email" value={this.state.input} onChange={this.updateInput} />
+              <input type="text" placeholder="contraseña" />
+
+              <input type="submit" value="Submit" />
+            </form>
+          </header>
+        </div>
+      );
+    }
   }
 }
 
