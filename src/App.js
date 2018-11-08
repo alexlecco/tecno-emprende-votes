d9eb71eefd5d378ab644ed4af8d50b922ad0133b
alex.villecco <alex.villecco@globant.com>
2018-11-08 09:51:01 -0300

    Implement login by email

--- src/App.js
+++ src/App.js
@@ -9,9 +9,13 @@ class App extends Component {
     super(props);
     this.state = {
       logged: false,
-      loggedJury: "",
+      loggedJury: {
+        id: '',
+        name: '',
+        remaining_stars: 0,
+      },
       input: '',
-      value: ''
+      value: '',
     }
     this.handleSubmit = this.handleSubmit.bind(this);
     this.updateInput = this.updateInput.bind(this);
@@ -25,10 +29,41 @@ class App extends Component {
       value: this.state.input,
       logged: true
     });
+    this.selectJury(this.juriesRef);
   }
 
   updateInput(event) {
     this.setState({input: event.target.value});
+  }
+
+  getObjectOfArray(array, index) {
+    return array[index] = array[index] || {};
+  }
+
+  selectJury(juriesRef) {
+    juriesRef.on('value', (snap) => {
+      const juryEmail = this.state.value;
+      let juries = [];
+
+      snap.forEach((child) => {
+        if(child.val().email === juryEmail) {
+          juries.push({
+            name: child.val().name,
+            remaining_stars: child.val().remaining_stars,
+            id: child.val().id,
+            _key: child.key,
+          });
+        }
+      });
+
+      this.setState({
+        loggedJury: {
+          name: this.getObjectOfArray(juries, 0).name,
+          remaining_stars: this.getObjectOfArray(juries, 0).remaining_stars,
+          id: this.getObjectOfArray(juries, 0).id,
+        }
+      })
+    });
   }
 
   render() {
@@ -38,7 +73,7 @@ class App extends Component {
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <p>
-              Bienvenido { this.state.value }
+              Bienvenido { this.state.loggedJury.name }
             </p>    
           </header>
         </div>
@@ -52,7 +87,6 @@ class App extends Component {
             </p>
             <form onSubmit={this.handleSubmit}>
               <input type="text" placeholder="email" value={this.state.input} onChange={this.updateInput} />
-              <input type="text" placeholder="contraseña" />
 
               <input type="submit" value="Submit" />
             </form>
